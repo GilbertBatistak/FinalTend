882c1c0d12151d15ff255ba82172b8cce7c76b17
GilbertBatistak <GilbertBatista.k@gmail.com>
2020-01-15 01:42:21 -0400

    FinalVersion

--- FinalTend.py
+++ FinalTend.py
@@ -1,5 +1,9 @@
 import unittest
 import collections
+import os.path
+import os
+import sys
+from os import path
 
 def IsAnagram(lis):
     count = 0
@@ -37,4 +41,24 @@ class MyAnagramTests(unittest.TestCase):
         a=["paste", "pates", "kinship", "pinkish" ,"enlist", "inlets", "boaster" ,"boaters"]
         self.assertEqual(IsAnagram(a), 4)
     
-unittest.main()
+    def test_EverythingIsDifferentThereforeNoAnagrams(self):
+        a=["pas", "pes", "kinsp", "pkish" ,"nlist", "inles", "boste" ,"boater"]
+        self.assertEqual(IsAnagram(a), 0)
+    
+
+path = sys.argv[1]
+result = []
+if os.path.exists(path):
+    os.path.exists(path)
+    with open(path) as fp:
+        line = fp.readline()
+        cnt = 0
+        sortedList = []
+        while line:
+            result.append(line)
+            line = fp.readline()
+            cnt += 1
+    result = [s.rstrip() for s in result]
+    print(IsAnagram(result))
+else: 
+    print("File not specified/found")
